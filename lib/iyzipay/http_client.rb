5478a4f5bea233130533f2c94885e4fcd93c20ba
Sinan Keskin <sinan@kesk.in>
2019-08-16 23:55:39 +0300

    Remove to_s from http client methods

--- lib/iyzipay/http_client.rb
+++ lib/iyzipay/http_client.rb
@@ -3,19 +3,19 @@
 module Iyzipay
   class HttpClient
     def self.get(path, options = {}, &block)
-      HTTParty.get(path, options, &block).to_s
+      HTTParty.get(path, options, &block)
     end
 
     def self.post(path, options = {}, &block)
-      HTTParty.post(path, options, &block).to_s
+      HTTParty.post(path, options, &block)
     end
 
     def self.put(path, options = {}, &block)
-      HTTParty.put(path, options, &block).to_s
+      HTTParty.put(path, options, &block)
     end
 
     def self.delete(path, options = {}, &block)
-      HTTParty.delete(path, options, &block).to_s
+      HTTParty.delete(path, options, &block)
     end
   end
 end
